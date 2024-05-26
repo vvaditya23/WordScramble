337d5e2ca0b15169a4ba5675a8aa94d779ab4595
Aditya Vyavahare <vvaditya23@icloud.com>
2024-05-26 13:55:26 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aditya Vyavahare on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
